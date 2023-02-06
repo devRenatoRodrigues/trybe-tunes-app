56ecfb9b93d978ede7aa9bbb42ca59230e4e1861
devRenatoRodrigues <rehrlz@hotmail.com>
2023-02-06 17:42:50 -0300

    exercicio 5 completo

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    disabled: true,
+  };
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    const minLength = 2;
+    const nameLength = value.length;
+    if (nameLength >= minLength) {
+      this.setState({
+        disabled: false,
+      });
+    }
+  };
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="searchArtist">
+            Search for artist or band:
+            <input
+              data-testid="search-artist-input"
+              name="searchArtist"
+              type="text"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            disabled={ disabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
